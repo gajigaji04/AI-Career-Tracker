c26dcb69b664a2bee4e8deda6bbea612e33de5e0
gajigaji04 <gajigaji04@naver.com>
2026-06-16 18:19:22 +0900

    feat: React query hook으로 대시보드 사용자 정보 표시 확인

---
+++ frontend/src/hooks/useMe.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getMe } from "../api/auth";
+
+export const useMe = () => {
+  return useQuery({
+    queryKey: ["me"],
+
+    queryFn: getMe,
+  });
+};
